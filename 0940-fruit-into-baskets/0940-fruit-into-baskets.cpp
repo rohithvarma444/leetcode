ec3a8bd7375bbb95dd5e025d29d5723eb94eb58e
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2025-07-12 21:22:03 +0530

    Time: 47 ms (33.51%), Space: 75.1 MB (56.9%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        
         int n = fruits.size();
-        map<int,int> mpp;
-        int left = 0; 
+        int left = 0;
         int right = 0;
-        int ans = 0;
 
+        unordered_map<int,int> mpp;
+        int ans = 0;
 
         while(right < n){
             mpp[fruits[right]]++;
-            
-            while(mpp.size() > 2 && left <= right){
+            while(mpp.size() > 2){
                 mpp[fruits[left]]--;
                 if(mpp[fruits[left]] == 0) mpp.erase(fruits[left]);
                 left++;
             }
-
-            ans = max(ans, right - left + 1);
+            ans = max(ans,right-left+1);
             right++;
         }
 
