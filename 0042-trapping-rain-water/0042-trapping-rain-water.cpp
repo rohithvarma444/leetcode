6bde85d4e1d89a9c745a3d6af6e3038d5b8eca9f
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2025-07-21 11:56:13 +0530

    Time: 0 ms (100%), Space: 26.1 MB (60.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-         int n = height.size();
-        vector<int> prefixMax(n);
-        vector<int> suffixMax(n);
+    int trap(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int res = 0;
 
-       
-        int currMax = height[0];
-
-        for(int i = 0; i < n; i++){
-            currMax = max(currMax,height[i]);
-            prefixMax[i] = currMax;
+        while (left < right) {
+            if (nums[left] < nums[right]) {
+                if (nums[left] >= leftMax) {
+                    leftMax = nums[left];
+                } else {
+                    res += leftMax - nums[left];
+                }
+                ++left;
+            } else {
+                if (nums[right] >= rightMax) {
+                    rightMax = nums[right];
+                } else {
+                    res += rightMax - nums[right];
+                }
+                --right;
+            }
         }
-
-        currMax = height[n-1];
-        for(int i = n-1; i >= 0; i--){
-            currMax = max(currMax,height[i]);
-            suffixMax[i] = currMax;
-        }
-
-        int totalWater = 0;
-
-        for(int i = 0; i < n; i++){
-            int currWater = min(prefixMax[i],suffixMax[i]) - height[i];
-            if(currWater > 0) totalWater += currWater;
-        }
-
-        return totalWater;
+        return res;
     }
 };
