42a2176abb58582f7c734988593a4e352e980752
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2025-07-29 11:37:53 +0530

    Time: 0 ms (100%), Space: 59.3 MB (80.96%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -1,39 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         TreeNode* newN = new TreeNode(val);
-        if(root == nullptr) return newN;
 
-        TreeNode* curr = root;
+        if (root == nullptr)
+            return newN;
 
-        while(curr != nullptr){
-            if(curr->val > val && curr->left != nullptr){
-                curr = curr->left;
-            }
-
-            if(curr->val < val && curr->right != nullptr){
-                curr = curr->right;
-            }
-
-            if(curr->left == nullptr && curr->val > val){
-                curr->left = newN;
-                return root;
-            }
-
-            if(curr->right == nullptr && curr->val < val){
-                curr->right = newN;
-                return root;
+        TreeNode* temp = root;
+        while (true) {
+            if (val < temp->val) {
+                if (temp->left == nullptr) {
+                    temp->left = newN;
+                    break;
+                } else {
+                    temp = temp->left;
+                }
+            } else {
+                if (temp->right == nullptr) {
+                    temp->right = newN;
+                    break;
+                } else {
+                    temp = temp->right;
+                }
             }
         }
 
