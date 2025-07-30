6caca871f01e6a4f113c960ef126e530b2e294d5
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2025-07-30 14:54:31 +0530

    Time: 0 ms (100%), Space: 15 MB (61.52%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        vector<vector<int>> dp(m, vector<int>(n,-1));
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        for(int i =0; i < n; i++){
-            dp[m-1][i] = matrix[m-1][i];
+        for(int i = 0; i < n; i++){
+            dp[0][i] = matrix[0][i];
         }
 
-        for(int i = m-2; i >= 0; i--){
+        for(int i = 1; i < n; i++){
             for(int j = 0; j < n; j++){
-
-                int below = dp[i+1][j] + matrix[i][j];
-                int diagL = INT_MAX;
-                if(j > 0){
-                    diagL = dp[i+1][j-1] + matrix[i][j];
-                }
-                int diagR = INT_MAX;
-                if( j!= n-1){
-                    diagR = dp[i+1][j+1] + matrix[i][j];
-                }
-
-
-                dp[i][j] = min(diagL,min(diagR,below));
+                int up = dp[i-1][j];
+                int upLeft = (j-1 >= 0) ? dp[i-1][j-1] : INT_MAX;
+                int upRight = (j+1 < n) ? dp[i-1][j+1] : INT_MAX;
+                
+                dp[i][j] = matrix[i][j] + min({up, upLeft, upRight});
             }
         }
 
-
-        int ans = dp[0][0];
-        for(int i =0; i < n; i++){
-            ans = min(ans,dp[0][i]);
+        int result = dp[n-1][0];
+        for(int j = 1; j < n; j++){
+            result = min(result, dp[n-1][j]);
         }
-
-        return ans;
+        
+        return result;
     }
 };
