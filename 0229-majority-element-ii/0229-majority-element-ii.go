e4e7dd4dfd628c359a26338a50f3e3c892616c0d
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2025-08-08 20:30:58 +0530

    Time: 0 ms (100%), Space: 8.1 MB (66.04%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.go
@@ -0,0 +1,43 @@
+func majorityElement(nums []int) []int {
+    n := len(nums)
+    cnt1, cnt2 := 0, 0
+    var elem1, elem2 int
+    elem1, elem2 = 0, 1e9 
+
+    for i := 0; i < n; i++ {
+        if nums[i] == elem1 {
+            cnt1++
+        } else if nums[i] == elem2 {
+            cnt2++
+        } else if cnt1 == 0 {
+            elem1 = nums[i]
+            cnt1 = 1
+        } else if cnt2 == 0 {
+            elem2 = nums[i]
+            cnt2 = 1
+        } else {
+            cnt1--
+            cnt2--
+        }
+    }
+
+    cnt1, cnt2 = 0, 0
+
+    for i := 0; i < n; i++ {
+        if nums[i] == elem1 {
+            cnt1++
+        } else if nums[i] == elem2 {
+            cnt2++
+        }
+    }
+
+    result := []int{}
+    if cnt1 > n/3 {
+        result = append(result, elem1)
+    }
+    if cnt2 > n/3 {
+        result = append(result, elem2)
+    }
+
+    return result
+}
