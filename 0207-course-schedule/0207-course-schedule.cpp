cd4d21e323855479b5261eed5a367cca334b57aa
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2025-08-28 09:28:15 +0530

    Time: 9 ms (29.61%), Space: 19.9 MB (48.36%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& pre) {
-        vector<int> ans;
-        queue<int> q;
 
         vector<vector<int>> adj(numCourses);
+        vector<int> inOrder(numCourses,0);
 
         for(auto it: pre){
-            adj[it[1]].push_back(it[0]);
+            int u = it[0];
+            int v = it[1];
+            adj[v].push_back(u);
+            inOrder[u]++;
         }
 
-        vector<int> inOrder(numCourses,0);
-        for(auto it: adj){
-            for(auto at: it){
-                inOrder[at]++;
-            }
-        }
+        queue<int> q;
 
         for(int i = 0; i < numCourses; i++){
             if(inOrder[i] == 0){
@@ -24,21 +21,21 @@ public:
         }
 
         while(!q.empty()){
-
             int curr = q.front();
             q.pop();
-            ans.push_back(curr);
 
             for(auto it: adj[curr]){
                 inOrder[it]--;
                 if(inOrder[it] == 0){
                     q.push(it);
                 }
-            }
+            }            
         }
-        
-        return ans.size() == numCourses;
-    }
 
-    
+        for(auto it: inOrder){
+            if(it != 0) return false;
+        }
+        return true;
+        
+    }
 };
