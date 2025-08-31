01fe56e76cace41c67b03c6d917b2ec773b42f82
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2025-08-31 20:23:35 +0530

    Time: 4 ms (16.23%), Space: 15 MB (27.26%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
+    void solve(vector<vector<char>>& grid) {
+        int m = grid.size();
+        if(m == 0) return; 
+        int n = grid[0].size();
+        vector<vector<int>> vis(m, vector<int>(n,0));
 
         queue<pair<int,int>> q;
-        vector<vector<int>> vis(m, vector<int>(n,0));
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0 || i == m-1 || j == n-1){
+                    if(grid[i][j] == 'O' && !vis[i][j]){
+                        q.push({i,j});
+                        vis[i][j] = 1;
+                    } 
+                }
+            }
+        }
 
         vector<int> dr = {-1,0,1,0};
         vector<int> dc = {0,-1,0,1};
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if( i == 0 || j == 0 || i == m-1 || j == n-1){
-                    if(board[i][j] == 'O'){
-                        q.push({i,j});
-                        vis[i][j] = 1;
-                    }
-                }
-            }
-        }
-
         while(!q.empty()){
             int currX = q.front().first;
             int currY = q.front().second;
-
             q.pop();
-            for(int i = 0; i < 4; i++){
-                int nr = currX + dr[i];
-                int nc = currY + dc[i];
 
-                if(nr >= 0 && nc >= 0 && nr < m && nc < n && board[nr][nc] == 'O'){
-                    if(!vis[nr][nc]){
-                        q.push({nr,nc});
-                        vis[nr][nc] = 1;
-                    }
+            for(int i = 0; i < 4; i++){
+                int newX = currX + dr[i];
+                int newY = currY + dc[i];
+
+                if(newX >= 0 && newY >= 0 && newX < m && newY < n 
+                   && grid[newX][newY] == 'O' && !vis[newX][newY]){
+                    vis[newX][newY] = 1;
+                    q.push({newX,newY});
                 }
             }
         }
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(vis[i][j] == 0 && board[i][j] == 'O'){
-                    board[i][j] = 'X';
+                if(grid[i][j] == 'O' && !vis[i][j]){
+                    grid[i][j] = 'X';
                 }
             }
         }
-
-        
     }
 };
