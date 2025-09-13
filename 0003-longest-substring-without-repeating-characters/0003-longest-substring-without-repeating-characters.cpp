e905ff9b2bbab81868eb151e8746b9e3a2e02927
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2025-09-13 17:41:01 +0530

    Time: 19 ms (29.54%), Space: 13.6 MB (38.42%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,23 +4,23 @@ public:
         int n = s.size();
         int left = 0;
         int right = 0;
-
         map<char,int> mpp;
-        int maxi = 0;
+
+        int ans = 0;
 
         while(right < n){
             mpp[s[right]]++;
 
-            while(mpp[s[right]] > 1 && left <= right){
+            while(mpp[s[right]] > 1){
                 mpp[s[left]]--;
                 if(mpp[s[left]] == 0) mpp.erase(s[left]);
                 left++;
             }
 
-            maxi = max(maxi,right-left+1);
+            ans = max(ans, right - left + 1);
             right++;
         }
 
-        return maxi;
+        return ans;
     }
 };
