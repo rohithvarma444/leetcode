ac617d1a4d6d88dda3cc73c474f89945e3645c02
Rohith Varma Datla <111046073+rohithvarma444@users.noreply.github.com>
2025-10-05 18:05:45 +0530

    Time: 0 ms (100%), Space: 28.2 MB (28.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,18 +3,20 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
 
-        int candidate = nums[0];
+        int ele = nums[0];
         int cnt = 1;
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] == candidate) cnt++;
-            else cnt--;
+        for(int i = 1; i < n; i++){
             if(cnt == 0){
-                candidate = nums[i];
-                cnt++;
+                ele = nums[i];
+                cnt = 1;
             }
+            else if(nums[i] != ele){
+                cnt--;
+            }
+            else if(nums[i] == ele) cnt++;
         }
 
-        return candidate;
+        return ele;
     }
 };
